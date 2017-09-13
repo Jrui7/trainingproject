341eb985e8f016debcac62ced3cc00ccbdb1fe7d
jorge ruivinho <jorge.ruivinho@me.com>
2017-09-13 15:48:59 +0200

    my seedoak has his own controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :seeds, shallow: true do
-    collection do                       # collection => no restaurant id in URL
-      get 'my_seedoak', to: "seeds#my_seedoak"
-      get 'my_seeds', to: "seeds#my_seeds"
-    end
     resources :picks
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [ :show, :edit, :update, :destroy]
+
+  get 'feed', to: 'my_seedoak#feed'
+  get 'my_seeds', to: 'my_seedoak#my_seeds'
+
 end
