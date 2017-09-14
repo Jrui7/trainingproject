f5be0c8bf365b3c64481ea228ff2bc866e7e15ec
jorge ruivinho <jorge.ruivinho@me.com>
2017-09-14 19:07:50 +0200

    remove user preference

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,9 @@ Rails.application.routes.draw do
     resources :picks
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [ :show, :edit, :update, :destroy], shallow: true do
-    resources :user_preferences, only: [:new, :create, :destroy]
-  end
+  resources :users, only: [ :show, :edit, :update, :destroy], shallow: true
 
 
-  get 'feed', to: 'my_seedoak#feed'
   get 'my_seeds', to: 'my_seedoak#my_seeds'
   get 'my_picks', to: 'my_seedoak#my_picks'
 
