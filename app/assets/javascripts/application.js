2f6b9221f7499883486dae9f2a722b5b4864a444
jorge ruivinho <jorge.ruivinho@me.com>
2017-10-17 11:55:38 +0200

    first version of the animated picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,37 @@
 //= require owl.carousel
 
 //= require_tree.
+
+$( "#start" ).keyup(function() {
+    var value = $( this ).val();
+
+
+    $('.counter').each(function() {
+    var $this = $(this),
+        countTo = value;
+
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+
+    {
+
+      duration: 500,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+
+    });
+
+
+
+  });
+  })
+  .keyup();
+
+
