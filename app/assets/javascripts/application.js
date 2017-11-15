6b531eed9f6e41a347d7c8bf974c2907b8673fbe
jorge ruivinho <jorge.ruivinho@me.com>
2017-11-15 19:04:17 +0100

    pick in a modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require bootstrap
 
 //= require jquery-fileupload/basic
--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -4,26 +4,14 @@ class PicksController < ApplicationController
     @picks = current_user.picks
   end
 
-  def new
-    @pick = Pick.new
-    @seed = Seed.find(params[:seed_id])
-    @user = current_user
-    @pick.user = current_user
-    @pick.seed = Seed.find(params[:seed_id])
-
-  end
 
   def create
-    @pick = Pick.new(pick_params)
-    @pick.save
+    @seed = Seed.find(params[:seed_id])
+    @pick = @seed.picks.new(pick_params)
+    if @pick.save
       @seed.increment_popularity
-      respond_to do |format|
-        format.html { redirect_to seed_path(@seed) }
-        format.js
-      end
-
-
-
+      redirect_to seed_path(@seed)
+    end
   end
 
   def destroy
