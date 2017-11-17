6b13487f8596234c1e9017e2d375c09143222d76
jorge ruivinho <jorge.ruivinho@me.com>
2017-11-17 21:31:53 +0100

    ajax works on signal but not on unsignal

--- app/controllers/signal_seeds_controller.rb
+++ app/controllers/signal_seeds_controller.rb
@@ -1,16 +1,21 @@
 class SignalSeedsController < ApplicationController
 
   def create
-    seed = Seed.find(params[:seed_id])
-    seed.signal_seed.create! user: current_user
-    redirect_to seed_path(seed)
+    @seed = Seed.find(params[:seed_id])
+    @seed.signal_seed.create! user: current_user
+    respond_to do |format|
+      format.html {redirect_to seed_path(@seed)}
+      format.js
+    end
   end
 
   def destroy
-    seed = Seed.find(params[:id])
+    @seed = Seed.find(params[:id])
     unsignal = SignalSeed.find(params[:id])
     unsignal.destroy
-    redirect_to seed_path(seed)
+    respond_to do |format|
+      format.html {redirect_to seed_path(@seed)}
+    end
   end
 
 end
