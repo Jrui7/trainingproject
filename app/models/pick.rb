727d8b2cb0e4f512c32329a930d2d0dbf452d84b
jorge ruivinho <jorge.ruivinho@me.com>
2017-12-01 10:52:14 +0100

    export csv with client seed and pick details works

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -12,12 +12,12 @@ class Pick < ApplicationRecord
   scope :newest, -> { order(created_at: :desc)}
 
   def self.all_with_seed_details
-      Pick.select("picks.*, seeds.title as seed_title").joins(:seed)
+      Pick.select("picks.*, seeds.title as seed_title, users.first_name as user_first_name").joins(:seed, :user)
   end
 
   def self.as_csv
     CSV.generate do |csv|
-      columns = %w(id price seed_title)
+      columns = %w(id price seed_title user_first_name)
       csv << columns.map(&:humanize)
       all_with_seed_details.each do |pick|
         csv << pick.attributes.values_at(*columns)
