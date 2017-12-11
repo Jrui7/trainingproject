bb63621e0499405c3108c373b033812fbb75c7bb
jorge ruivinho <jorge.ruivinho@me.com>
2017-12-11 19:25:10 +0100

    form updated with sex and birthdate infos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo, :sex])
   end
 
   def after_sign_in_path_for(resource_or_scope)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
          validates :pseudo, uniqueness: true
+         validates :sex, presence: true
 
          has_many :seeds
          has_many :picks
