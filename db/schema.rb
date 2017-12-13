c8fdbebf5bce0d5df4c1cda64e4ce17279003da9
jorge ruivinho <jorge.ruivinho@me.com>
2017-12-13 18:29:49 +0100

    responsive vendors infos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213113008) do
+ActiveRecord::Schema.define(version: 20171213165752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 20171213113008) do
     t.datetime "expiration"
     t.integer  "category_id"
     t.integer  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.integer  "popularity"
     t.string   "url"
     t.string   "secondary_url"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20171213113008) do
     t.string   "sale_point_url"
     t.string   "sale_point_address"
     t.string   "sale_point_contact"
+    t.text     "official_description"
     t.index ["category_id"], name: "index_seeds_on_category_id", using: :btree
     t.index ["user_id"], name: "index_seeds_on_user_id", using: :btree
   end
