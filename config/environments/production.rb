be6a8af952c0cb696e350b30d1e2c10505e934c4
jorge ruivinho <jorge.ruivinho@me.com>
2018-01-04 13:38:13 +0100

    change config prod for https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   # ...
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "seedlap.com" }
+  config.action_mailer.default_url_options = { host: "https://www.seedlap.com" }
 
 
   # Code is not reloaded between requests.
