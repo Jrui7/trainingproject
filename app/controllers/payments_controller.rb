c5fb5c7c1eefd8a547d211178107522422946e2c
jorge ruivinho <jorge.ruivinho@me.com>
2018-01-09 18:25:45 +0100

    re-indending previous commit

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -11,47 +11,47 @@ class PaymentsController < ApplicationController
 
     def create
 
-        if current_user.customer_id.blank?
-          customer = Stripe::Customer.create(
-            source: params[:stripeToken],
-            email:  params[:stripeEmail]
-          )
-
-          customer_id = customer.id
-          current_user.customer_id = customer_id
-          current_user.address = params["stripeBillingAddressLine1"]
-          current_user.zip_code = params["stripeBillingAddressZip"]
-          current_user.city = params["stripeBillingAddressCity"]
-          current_user.last_name = params["stripeBillingName"]
-          current_user.save
-
-        else
-          if params[:stripeToken]
-            cu = Stripe::Customer.retrieve("#{current_user.customer_id}")
-            cu.source = params[:stripeToken] # obtained with Stripe.js
-            cu.save
-            customer_id = current_user.customer_id
-            current_user.customer_id = customer_id
-            current_user.save
-          else
-            customer_id = current_user.customer_id
-          end
-        end
-
-
-        charge = Stripe::Charge.create(
-          customer:     customer_id,   # You should store this customer id and re-use it.
-          amount:       @pick.amount_cents,
-          description:  "Participation au deal pour #{@pick.seed.title} enregistrée",
-          currency:     @pick.amount.currency
+      if current_user.customer_id.blank?
+        customer = Stripe::Customer.create(
+          source: params[:stripeToken],
+          email:  params[:stripeEmail]
         )
 
-        @pick.update(payment: charge.to_json, state: 'paid')
-        redirect_to pick_path(@pick)
+        customer_id = customer.id
+        current_user.customer_id = customer_id
+        current_user.address = params["stripeBillingAddressLine1"]
+        current_user.zip_code = params["stripeBillingAddressZip"]
+        current_user.city = params["stripeBillingAddressCity"]
+        current_user.last_name = params["stripeBillingName"]
+        current_user.save
 
-        rescue Stripe::CardError => e
-        flash[:alert] = e.message
-        redirect_to new_pick_payment_path(@pick)
+      else
+        if params[:stripeToken]
+          cu = Stripe::Customer.retrieve("#{current_user.customer_id}")
+          cu.source = params[:stripeToken] # obtained with Stripe.js
+          cu.save
+          customer_id = current_user.customer_id
+          current_user.customer_id = customer_id
+          current_user.save
+        else
+          customer_id = current_user.customer_id
+        end
+      end
+
+
+      charge = Stripe::Charge.create(
+        customer:     customer_id,   # You should store this customer id and re-use it.
+        amount:       @pick.amount_cents,
+        description:  "Participation au deal pour #{@pick.seed.title} enregistrée",
+        currency:     @pick.amount.currency
+      )
+
+      @pick.update(payment: charge.to_json, state: 'paid')
+      redirect_to pick_path(@pick)
+
+      rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_pick_payment_path(@pick)
 
 
     end
