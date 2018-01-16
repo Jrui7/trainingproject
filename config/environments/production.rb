dc4d5ab7a8c68ac9ab07a3a0c533f4f8737e3df8
jorge ruivinho <jorge.ruivinho@me.com>
2018-01-16 21:12:35 +0100

    change for mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # ...
-  config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
+  config.action_mailer.delivery_method = :mailjet
   config.action_mailer.default_url_options = { host: "https://www.seedlap.com" }
+
+
 
 
   # Code is not reloaded between requests.
