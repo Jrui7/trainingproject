95b36007e289fdf70a41d8daeca6c34ed5afdc1d
jorge ruivinho <jorge.ruivinho@me.com>
2018-01-18 18:12:21 +0100

    includes(:user) in finalize campaign method to optimize sql request, declaration of payment hash variable to do only one sql request used 3 times in the method

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,8 +2,10 @@ class Campaign < ApplicationRecord
   belongs_to :seed
 
   def finalize_campaign
-    self.seed.picks.each do |pick|
+    self.seed.picks.includes(:user).each do |pick|
       if pick.state == "paid"
+        payment_hash = JSON.parse(pick.payment)
+
         if self.status == "success"
           if pick.price >= self.price
             customer_id = pick.user.customer_id
@@ -17,7 +19,6 @@ class Campaign < ApplicationRecord
             pick.update(deal_price: charge.to_json)
             final_payment = JSON.parse(pick.deal_price)
             if final_payment["paid"] == true
-              payment_hash = JSON.parse(pick.payment)
                Stripe::Refund.create(
                  charge: payment_hash["id"]
                )
@@ -26,19 +27,18 @@ class Campaign < ApplicationRecord
               pick.update(state: "error")
             end
           else
-            payment_hash = JSON.parse(pick.payment)
              Stripe::Refund.create(
                charge: payment_hash["id"]
              )
             pick.update(state: "pick_failed")
           end
         else
-          payment_hash = JSON.parse(pick.payment)
            Stripe::Refund.create(
              charge: payment_hash["id"]
            )
           pick.update(state: "campaign_failed")
         end
+
       else
         pick.update(state: "campaign_closed")
       end
