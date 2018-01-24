0362e5eb56a19f9c7a0256fb0bfdb7561dc986a6
jorge ruivinho <jorge.ruivinho@me.com>
2018-01-24 12:42:18 +0100

    counter works with two seperate functions

--- app/assets/javascripts/create-pick.js
+++ app/assets/javascripts/create-pick.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-
-   counter();
+  var target = $('#start-pick');
+  var counter = $(".counter");
+  setCounterValueAndAction(target, counter);
 
   });
 
@@ -42,6 +43,55 @@ function counter() {
     }
   })
 }
+
+function countTo(target, counter) {
+  var target = $(target).val();
+  $(counter).each(function() {
+    var $this = $(this),
+      countTo = target;
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 1500,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+    });
+  });
+}
+
+function setCounterValueAndAction(target, counter) {
+  $(target).on('keyup', function(event) {
+    if (event.keyCode == 8) {
+      if ($(target).val() == "") {
+        $(counter).html("€");
+      }
+      else {
+        var newTarget = $(target).val();
+        $(counter).html(newTarget);
+      }
+    }
+    else if (event.keyCode != 13) {
+      $(counter).html("");
+      var newCounter = $(counter);
+      countTo(target, newCounter);
+    }
+  })
+}
+
+
+
+// compter si retour arrière et valeur != ""
+
+// compter si keyup != retour arrière et enter
+
+// clear counter si keyup == retour arrière et val == ""
+
 
 
 
