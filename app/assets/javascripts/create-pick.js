28f73d71fb8774527f899cc0e20417eb75e8a782
jorge ruivinho <jorge.ruivinho@me.com>
2018-01-24 14:17:33 +0100

    style counter

--- app/assets/javascripts/create-pick.js
+++ app/assets/javascripts/create-pick.js
@@ -6,43 +6,7 @@ $(document).ready(function() {
   });
 
 
-function counter() {
-  $('#start-pick').on('keyup', function(event) {
-    if (event.keyCode == 8) {
-      if ($('#start-pick').val() == "") {
-        $('.counter').html("€");
-      }
-    }
-    else if (event.keyCode != 13) {
-      var value = $( this ).val();
-        $('.counter').html("");
-      $('.counter').each(function() {
-      var $this = $(this),
-          countTo = value;
 
-      $({ countNum: $this.text()}).animate({
-          countNum: countTo
-        },
-
-      {
-
-        duration: 1500,
-        easing:'linear',
-        step: function() {
-          $this.text(Math.floor(this.countNum));
-        },
-        complete: function() {
-          $this.text(this.countNum);
-        }
-
-      });
-
-
-
-      });
-    }
-  })
-}
 
 function countTo(target, counter) {
   var target = $(target).val();
