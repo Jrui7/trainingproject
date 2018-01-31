7e3af9be0cfccf5688cab07a1962b9fa521d0b19
jorge ruivinho <jorge.ruivinho@me.com>
2018-01-31 19:19:07 +0100

    scroll reveal working on homepage how it works

---
+++ app/assets/javascripts/scroll-reveal.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  window.sr = ScrollReveal({ reset: true });
+  sr.reveal('.icon-header-body', {
+    duration: 1000,
+    origin: 'top',
+    reset: true,
+  });
+})
+
