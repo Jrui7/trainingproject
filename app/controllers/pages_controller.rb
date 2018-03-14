55955339c00b8b6e9295f4b06631150494389f0b
jorge <jorge.ruivinho@me.com>
2018-03-14 18:32:58 +0100

    faq cgv cgu contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,30 @@
 class PagesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:home]
-  before_action :set_sample, only: [:faq, :team]
+  before_action :set_sample
 
 
   def home
   end
 
-  def faq
+  def infos
   end
 
-  def team
+  def faq
+    @page = params["action"]
+  end
+
+  def contact
+  end
+
+  def mentions_legales
+    @page = params["action"]
+  end
+
+  def cgv
+  end
+
+  def cgu
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,9 +39,14 @@ Rails.application.routes.draw do
   get 'popular', to: 'seeds#popular'
   get 'last_day', to: 'seeds#last_day'
 
-  get 'faq', to: 'pages#faq'
   get 'home', to: 'pages#home'
-  get 'team', to: 'pages#team'
+  get 'infos', to: 'pages#infos'
+  get 'faq', to: 'pages#faq'
+  get 'contact', to: 'pages#contact'
+  get 'mentions_legales', to: 'pages#mentions_legales'
+  get 'CGV', to: 'pages#CGV'
+  get 'CGU', to: 'pages#CGU'
+
 
   get 'my_seeds', to: 'my_seedlap#my_seeds'
   get 'my_picks', to: 'my_seedlap#my_picks'
