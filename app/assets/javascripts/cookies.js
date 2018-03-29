e5409a5b5e451dd9f56c0e5dc931b5fa24202820
jorge <jorge.ruivinho@me.com>
2018-03-29 14:54:45 +0200

    change domain for neoju

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $(".cookies-eu-content-holder").text("En poursuivant votre navigation sur Seedlap, vous acceptez l'utilisation de cookies");
+  $(".cookies-eu-content-holder").text("En poursuivant votre navigation sur Neoju, vous acceptez l'utilisation de cookies");
 })
