91e1c62fffd45c346d3205c9484438e2acdb2379
jorge <jorge.ruivinho@me.com>
2018-04-12 13:49:22 +0200

    how it works and first page changes

--- app/assets/javascripts/log-xs.js
+++ app/assets/javascripts/log-xs.js
@@ -1,24 +1,7 @@
 $(document).ready(function(){
-  loadLogIn();
-  loadSignUp();
   connexion();
 })
 
-function loadLogIn() {
-  $("#sign-in").click(function(){
-    $("#action-xs").hide();
-    $(".form-xs").removeClass("hidden");
-    $(".form-login-xs").removeClass("hidden");
-  })
-}
-
-function loadSignUp() {
-  $("#sign-up").click(function(){
-    $("#action-xs").hide();
-    $(".form-xs").removeClass("hidden");
-    $(".form-sign-up-xs").removeClass("hidden");
-  })
-}
 
 function connexion() {
   $(".connect-button").click(function(){
