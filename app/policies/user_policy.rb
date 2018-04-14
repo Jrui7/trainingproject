bd7d1770ea3029c3edbb4dcd2527115b3c214026
jorge <jorge.ruivinho@me.com>
2018-04-14 13:06:21 +0200

    add onur in seed, allow admin to go seed and update user profile

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -6,11 +6,11 @@ class UserPolicy < ApplicationPolicy
   end
 
   def show?
-    record == user
+    record == user || user.admin == true
   end
 
   def update?
-    record == user
+    record == user || user.admin == true
   end
 
   def update_card?
