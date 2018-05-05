7ab1fae8ce60ecbc281480fda8ae20205685d7d0
jorge <jorge.ruivinho@me.com>
2018-05-05 13:56:24 +0200

    remove js code to hide button go-to how it works

--- app/assets/javascripts/log-xs.js
+++ app/assets/javascripts/log-xs.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 
 function connexion() {
   $(".connect-button").click(function(){
-      $(".go-to-link").hide()
       $(".connect-button").toggle();
       $(".log-in-home").removeClass("hidden");
   });
