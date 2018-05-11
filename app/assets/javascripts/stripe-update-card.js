5e513ac74495ae744aed28407a7721dd41223945
jorge <jorge.ruivinho@me.com>
2018-05-11 18:59:46 +0200

    save address if all fields are completed and then pay

--- app/assets/javascripts/stripe-update-card.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".display-card").click(function(){
-    $(".stripe-button-el").click();
-  })
-  $(".new-user-book").click(function(){
-    $(".stripe-button-el").click();
-  })
-})
--- app/assets/javascripts/update-user-address.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $(".user-update-form").click(function(){
-    $(".update-delivery-infos").removeClass("hidden");
-  })
-})
