bbca1a017ec4ee8d4cc4dd2192a85f785c09ed2b
jorge <jorge.ruivinho@me.com>
2018-06-05 13:45:02 +0200

    change neoju in some files

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $(".cookies-eu-content-holder").text("En poursuivant votre navigation sur Neoju, vous acceptez l'utilisation de cookies");
+  $(".cookies-eu-content-holder").text("En poursuivant votre navigation sur Pickalgo, vous acceptez l'utilisation de cookies");
 })
