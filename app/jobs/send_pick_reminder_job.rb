fc67f493a12c3c05a010a050f27a14fe1c217bb4
jorge <jorge.ruivinho@me.com>
2018-06-29 13:52:06 +0200

    change query synthax, error , not ; between conditions

--- app/jobs/send_pick_reminder_job.rb
+++ app/jobs/send_pick_reminder_job.rb
@@ -2,7 +2,7 @@ class SendPickReminderJob < ApplicationJob
   queue_as :default
 
   def perform
-    picks = Pick.where(reminder: false; state: "pending"; created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
+    picks = Pick.where(reminder: false, state: "pending", created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
     picks.each do |pick|
       PickreminderMailer.send_reminder_email(pick.id, pick.user_id)
       pick.reminder = true
