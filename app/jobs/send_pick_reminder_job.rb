75d9deeee529cb9c24faf3fcbe7e73948d18ffec
jorge <jorge.ruivinho@me.com>
2018-06-29 14:22:20 +0200

    working for picks with state pending, local and prod, now selecting picks pending created yesterday

--- app/jobs/send_pick_reminder_job.rb
+++ app/jobs/send_pick_reminder_job.rb
@@ -2,13 +2,11 @@ class SendPickReminderJob < ApplicationJob
   queue_as :default
 
   def perform
-    picks = Pick.where(reminder: false, state: "pending")
-    # , created_at: (Time.now.midnight - 1.day)..Time.now.midnight
+    picks = Pick.where(reminder: false, state: "pending", created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
     picks.each do |pick|
       PickreminderMailer.send_reminder_email(pick.id, pick.user_id).deliver_later
       pick.reminder = true
       pick.save
-      puts "email sent"
     end
   end
 end
