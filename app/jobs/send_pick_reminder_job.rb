2cf6b6922ece75417799eb746734d83a5143a982
jorge <jorge.ruivinho@me.com>
2018-07-03 18:34:34 +0200

    job working

--- app/jobs/send_pick_reminder_job.rb
+++ app/jobs/send_pick_reminder_job.rb
@@ -3,10 +3,12 @@ class SendPickReminderJob < ApplicationJob
 
   def perform(seed_id)
     seed = Seed.find(seed_id)
-    seed.picks.each do |pick|
+    picks = seed.picks.select {|pick| pick.state == "pending"}
+    picks.each do |pick|
       PickreminderMailer.send_reminder_email(pick.id, pick.user_id).deliver_later
       pick.reminder = true
       pick.save
     end
   end
+
 end
