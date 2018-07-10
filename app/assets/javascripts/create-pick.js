9533b22d16fe0fe61f25787e8c832f2dadb71acd
jorge <jorge.ruivinho@me.com>
2018-07-10 11:54:45 +0200

    counter works with euro

--- app/assets/javascripts/create-pick.js
+++ app/assets/javascripts/create-pick.js
@@ -2,7 +2,9 @@
 $(document).ready(function() {
   var target = $('#start-pick');
   var counter = $(".counter");
+  var totalcounter = $(".totalcounter");
   setCounterValueAndAction(target, counter);
+  setCounterValueAndActionTotal(target, totalcounter);
 
   });
 
@@ -30,6 +32,29 @@ function countTo(target, counter) {
   });
 }
 
+
+function countToTotal(target, totalcounter) {
+  var target = $(target).val();
+  $(totalcounter).each(function() {
+    var $this = $(this),
+      countTo = target;
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 1500,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        var finish = (this.countNum + 3.9).toString() + " " + "€";
+        $this.text(finish);
+      }
+    });
+  });
+}
+
 function setCounterValueAndAction(target, counter) {
   $(target).on('keyup', function(event) {
     if (event.keyCode == 8) {
@@ -49,6 +74,24 @@ function setCounterValueAndAction(target, counter) {
   })
 }
 
+function setCounterValueAndActionTotal(target, totalcounter) {
+  $(target).on('keyup', function(event) {
+    if (event.keyCode == 8) {
+      if ($(target).val() == "") {
+        $(totalcounter).html("€");
+      }
+      else {
+        var newTarget = $(target).val();
+        $(totalcounter).html(newTarget);
+      }
+    }
+    else if (event.keyCode != 13) {
+      $(totalcounter).html("");
+      var newCounter = $(totalcounter);
+      countToTotal(target, newCounter);
+    }
+  })
+}
 
 
 
