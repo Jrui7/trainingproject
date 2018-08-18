794e027f0cf7e65080f4d3cf3658615d7ab46e11
jorge <jorge.ruivinho@me.com>
2018-08-18 13:09:48 +0200

    save homepage new

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -4,14 +4,14 @@ class CampaignMailer < ApplicationMailer
     @user = User.find(user_id)
     @pick = Pick.find(pick_id)
     @campaign = Campaign.find(campaign_id)
-    mail to: @user.email, subject: "Erreur de paiement", from: 'contact@pickalgo.com'
+    mail to: @user.email, subject: "Erreur de paiement", from: '"Pickalgo" <contact@pickalgo.com>'
   end
 
   def pick_success(user_id, campaign_id, pick_id)
     @user = User.find(user_id)
     @campaign = Campaign.find(campaign_id)
     @pick = Pick.find(pick_id)
-    mail to: @user.email, subject: "Campagne d'achat concluante", from: 'contact@pickalgo.com'
+    mail to: @user.email, subject: "Campagne d'achat concluante", from: '"Pickalgo" <contact@pickalgo.com>'
   end
 
 
