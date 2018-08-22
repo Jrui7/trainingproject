93f0e1d2f8161f1f5193b44358b1473acf068975
jorge <jorge.ruivinho@me.com>
2018-08-22 11:04:55 +0200

    shake effect if no photo

---
+++ app/assets/javascripts/seed-new.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var form = document.getElementById('new_seed');
+  form.addEventListener('submit', function(event) {
+     event.preventDefault();
+     var photo = $(".attachinary_container").children('input').first();
+     if( !$(photo).val() ) {
+        $(".chooseFile").addClass("shake").css({ 'color': 'red',});
+      }
+      else {
+        form.submit();
+      }
+  })
+
+})
+
