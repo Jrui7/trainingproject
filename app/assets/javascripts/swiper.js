90001e68762f0624cefa79b3002fb6a49fb98732
jorge <jorge.ruivinho@me.com>
2018-09-04 23:05:41 +0200

    add swipper to carousel

---
+++ app/assets/javascripts/swiper.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  //Enable swiping...
+  $(".carousel").on("touchstart", function(event){
+          var xClick = event.originalEvent.touches[0].pageX;
+      $(this).one("touchmove", function(event){
+          var xMove = event.originalEvent.touches[0].pageX;
+          if( Math.floor(xClick - xMove) > 5 ){
+              $(this).carousel('next');
+          }
+          else if( Math.floor(xClick - xMove) < -5 ){
+              $(this).carousel('prev');
+          }
+      });
+      $(".carousel").on("touchend", function(){
+              $(this).off("touchmove");
+      });
+  });
+
+});
